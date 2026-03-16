862d4e58ec5ec4273ceb46565932c6cc1555010a
Smiles In Route <12345678+SmilesInRoute@users.noreply.github.com>
2026-03-15 20:44:50 -0700

    allow localhost:8080 cors for signup testing

--- index.js
+++ index.js
@@ -57,8 +57,9 @@ function parseAllowedOrigins() {
       "https://ops.smilesinroute.delivery",
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "http://localhost",
-"http://127.0.0.1",
+      "http://localhost:8080",
+      "http://127.0.0.1",
+
     ]);
   }
 
